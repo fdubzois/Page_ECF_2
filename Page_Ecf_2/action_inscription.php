689a9649665606b55de49eb4ac78501989830eac
fdubzois <119014187+fdubzois@users.noreply.github.com>
2023-02-19 23:50:14 +0100

    Update action_inscription.php

--- Page_Ecf_2/action_inscription.php
+++ Page_Ecf_2/action_inscription.php
@@ -1,45 +1,47 @@
 <?php
+if(isset($_POST['submit'])){
+  
+  // Créer variable
 
-// Créer variable
-
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "";
-
-
-$mail = $_POST['mail'];
-$password = $_POST['password'];
-$allergy = $_POST['allergy'];
-$Guest_Number = $_POST['Guest_Number'];
+  $servername = "localhost";
+  $username = "";
+  $password = "";
+  $dbname = "";
 
 
-// Créer une conexion
+  $mail = $_POST['mail'];
+  $password = $_POST['password'];
+  $allergy = $_POST['allergy'];
+  $Guest_Number = $_POST['Guest_Number'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// verifier la connexion
+  // Créer une conexion
 
-if ($conn->connect_error) {
-  die("La connexion a échouée: " . $conn->connect_error);
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // verifier la connexion
+
+  if ($conn->connect_error) {
+    die("La connexion a échouée: " . $conn->connect_error);
+  }
+
+  //Ajout dans Base de données
+
+  $sql = "INSERT INTO 'utilisateurs' ( 'mail', 'password', 'allergies', 'Guest_Number')
+  VALUES( $mail, $password, $allergy, $Guest_Number)
+  ";
+
+  // Vérification et message d'information
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Inscription effectué avec succés";
+  } else {
+    echo "Erreur: " . $sql . "
+  " . $conn->error;
+  }
+
+  $conn->close();
 }
-
-//Ajout dans Base de données
-
-$sql = "INSERT INTO 'utilisateurs' ( 'mail', 'password', 'allergies', 'Guest_Number')
-VALUES( $mail, $password, $allergy, $Guest_Number)
-";
-
-// Vérification et message d'information
-
-if ($conn->query($sql) === TRUE) {
-  echo "Inscription effectué avec succés";
-} else {
-  echo "Erreur: " . $sql . "
-" . $conn->error;
-}
-
-$conn->close();
-
+exit();
 
 ?>
